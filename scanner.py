0d8025703b98f7204a7e7f12784774cb95218f68
Eric Rion <erion42@gmail.com>
2026-08-08 22:10:22 -0400

    Add initial scanner.py with environmen test and colored output

---
+++ scanner.py
@@ -0,0 +1,9 @@
+#scanner.py
+#Main script for our home network logger project.
+
+#ANSI escape codes let us print colored text to the terminal.
+GREEN = "\033[92m"
+RESET = "\033[0m"
+
+# Print a test message
+print(f"{GREEN}[SUCCESS] Environment Initialized. Scanner is ready to build.{RESET}")
